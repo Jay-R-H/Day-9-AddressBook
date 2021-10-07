2c71cfa4e575c0c9e107eb89f3fac1a6c22801e4
Jay-R-H <jayhiremathr@gmail.com>
2021-10-07 12:30:32 +0530

    Added:Add contact details

--- src/com/programming/AddressBookMain.java
+++ src/com/programming/AddressBookMain.java
@@ -1,10 +1,43 @@
 package com.programming;
 
-public class AddressBookMain {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class AddressBookMain {
+    private static Scanner sc = new Scanner(System.in);
+    static ArrayList<ContactInfo> arrayPersonDetails = new ArrayList<>();
+
+    /*
+     * Method to Add contact details of a person and storing it an array.
+     */
+    public void addDetails() {
+        ContactInfo contact_details = new ContactInfo();
+
+        System.out.println("Enter First name: ");
+        contact_details.setFirstName(sc.next());
+        System.out.println("Enter Last name: ");
+        contact_details.setLastName(sc.next());
+        System.out.println("Enter Address: ");
+        contact_details.setAddress(sc.next());
+        System.out.println("Enter your City: ");
+        contact_details.setCity(sc.next());
+        System.out.println("Enter your State: ");
+        contact_details.setState(sc.next());
+        System.out.println("Enter Zip code: ");
+        contact_details.setZip(sc.nextLong());
+        System.out.println("Enter your Phone Number: ");
+        contact_details.setPhoneNumber(sc.nextLong());
+        System.out.println("Enter your E-mail: ");
+        contact_details.setEmail(sc.next());
+
+        arrayPersonDetails.add(contact_details);
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to address book program");
+        AddressBookMain final_details = new AddressBookMain();
+
+        final_details.addDetails();
     }
 
 }
